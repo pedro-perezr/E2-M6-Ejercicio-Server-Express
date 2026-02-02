1e7bc82f6f8e88fe4527cf6826cef59cca92c560
pedro-perezr <pedroperezruminot@gmail.com>
2026-02-01 23:45:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-// Importar la librería Express
 const express = require('express');
-
-// Crear una instancia de la aplicación Express
 const app = express();
-
-// Definir el puerto en el que escuchará el servidor
 const PORT = 3000;
 
-// Definir una ruta para la raíz (/) que responde a peticiones GET
 app.get('/', (req, res) => {
   res.send('Hola Mundo!');
 });
 
-// Iniciar el servidor y escuchar en el puerto definido
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en http://localhost:${PORT}`);
 });
